3ced12b41d5d7f7e9fa6e288617012c995a9a5ca
webmaster-dexontechnology <134662978+webmaster-dexontechnology@users.noreply.github.com>
2024-06-11 09:54:37 +0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup>
+const story = await useAsyncStoryblok('home', { version: 'draft' },
+{ customParent: 'https://app.storyblok.com' })
+</script>
+
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
